01ce99a3d455abda683b73417ed1d6b02abc593f
deepak9010 <deepak23072019@gmail.com>
2024-02-25 15:59:41 +0530

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,110 @@
+const info = {
+    main: {
+        name: "Deepak Singh",
+        description: "I am a final year B.Tech undergrad and a programmer. A passionate and dedicated developer.I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies likeReactJS, Material UI, NodeJS, ExpressJS. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Software Engineer",
+        role2: "MERN Developer",
+        photo: "../photo2.jpeg",
+        email: "deepak23072019@gmail.com",
+    },
+
+    socials: {
+        twitter: "https://twitter.com/DeepakSing24293",
+		github: "https://github.com/deepak9010",
+		linkedin: "https://www.linkedin.com/in/deepak-singh-8a49a5216/",
+		instagram: "https://www.instagram.com/deepaksingh123000/",
+        telegram: "https://t.me/deepakSingh_telegram"
+    },
+    
+    
+    projects: [
+        {
+			title: "Crud App",
+			description: "Design and developed a full-stack web application using the MERN stack to facilitate Create, Read,Update and Delete operations on data.",
+			technologies: "https://skillicons.dev/icons?i=react,nodejs,express,mongodb,0,0&perline=6&theme=",
+			github: "https://github.com/deepak9010/CRUD-mernapp",
+            link: "https://crud-mernapp-my77.vercel.app/all"
+		},
+
+		{
+			title: "CryptoBhaiya",
+			description: "A web App designed to track the prices and performance of various cryptocurrencies and exchanges in real time using crypto APIs.",
+			technologies: "https://skillicons.dev/icons?i=react,css,0,0&perline=5&theme=",
+			github: "https://github.com/deepak9010/CryptoBhaiyaa",
+            link: "https://crypto-bhaiya.vercel.app/"
+		},
+
+		{
+			title: "Portfolio",
+			description: "Created a Personal Portfolio website using HTML, CSS, and JavaScript, with enhanced interactivity using ReactJS.",
+			technologies: "https://skillicons.dev/icons?i=react,tailwindcss,0,0&perline=5&theme=",
+			github: "https://github.com/deepak9010/portfolio",
+            link: "https://portfolio-singhdeepak.vercel.app/"
+		},
+
+		{
+			title: "Weather App",
+			description: "Developed a user-friendly weather application using HTML, CSS, and JavaScript. Utilized external APIs to fetch real-time weather data for user-specified locations.",
+			technologies: "https://skillicons.dev/icons?i=html,css,javascript,0,0&perline=5&theme=",
+			github: "https://github.com/deepak9010/weatherApp",
+            link: "https://deepak9010.github.io/weatherApp/"
+		},
+    ],
+
+    skills: {
+        languages: {
+            description: "I possess proficient expertise in a diverse range of programming languages, including C, C++, JavaScript.",
+            image: "https://skillicons.dev/icons?i=c,cpp,cpp,javascript&perline=6&theme="
+        },
+        frameworks: {
+            description: "Experienced in a versatile tech stack, including React,Tailwind CSS, Material UI, Node.js, Express.js,and MongoDB.",
+            image: "https://skillicons.dev/icons?i=react,tailwind,materialui,nodejs,express,mongodb&perline=10&theme="
+        },
+        tools: {
+            description: "I am well-versed in utilizing various tools and platforms to enhance the development process, including Git, Github,Vercel, Postman.",
+            image: "https://skillicons.dev/icons?i=git,github,vercel,postman&perline=6&theme="
+        },
+    },
+
+    education: [
+        {
+            school: "Rajasthan Technical University, Kota",
+            degree: "B.Tech in Computer Science",
+            year: "2020 - Present",
+            image: "rtu.jpeg"
+        },
+        {
+            school: "Saraswati Vidya Mandir, Kosi kalan",
+            degree: "12th CBSE Board",
+            year: "2018 - 2020",
+            image: "svm.png"
+        },
+    ],
+
+    experience: [
+        {
+            company: "2DS App and Game Development Hub Pvt Limited",
+            domain: "Web Developer Intern",
+            duration: "May 2023 - June 2023",
+            image: "companylogo.png",
+            description: "->Actively participated in web development projects, collaborating with a diverse team to enhance userexperiences and functionalities. Utilized HTML, CSS, JavaScript and ReactJS to create responsive and visually appealing web interfaces."
+        },
+        {
+            company: "Bit-To-Byte Robotics",
+            domain: "frontend Developer Intern",
+            duration: "May 2022 - June 2022",
+            image: "companylogo.png",
+            description: "->Collaborated with the development team to create and maintain responsive, user-friendly web applications using HTML, CSS, and JavaScript"
+        },
+    
+    ],
+
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at deepak23072019@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    footer: "© 2024 Deepak Singh. All Rights Reserved"
+}
+
+export default info
